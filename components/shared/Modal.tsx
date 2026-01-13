fffd45a963dcc55b67058c15b49060180f740886
Ahmad-Software <dr.ahmad.salah.54@gmail.com>
2026-01-13 20:16:59 +0300

    refactor: remove drag-and-drop functionality from Modal component

--- components/shared/Modal.tsx
+++ components/shared/Modal.tsx
@@ -35,48 +35,13 @@ function Modal({
   maxWidth = "!max-w-2xl",
 }: ModalProps) {
   const { closeModal } = useModalStore();
-  const [position, setPosition] = useState({ x: 0, y: 0 });
-  const [isDragging, setIsDragging] = useState(false);
-  const [initialMousePosition, setInitialMousePosition] = useState({
-    x: 0,
-    y: 0,
-  });
-  const [initialPosition, setInitialPosition] = useState({ x: 0, y: 0 });
-  const handleMouseDown = (e: React.MouseEvent) => {
-    setIsDragging(true);
-    setInitialMousePosition({ x: e.clientX, y: e.clientY });
-    setInitialPosition({ x: position.x, y: position.y });
-  };
-  const handleMouseMove = (e: React.MouseEvent) => {
-    if (isDragging) {
-      const deltaMove = {
-        x: e.clientX - initialMousePosition.x,
-        y: e.clientY - initialMousePosition.y,
-      };
-      setPosition({
-        x: initialPosition.x + deltaMove.x,
-        y: initialPosition.y + deltaMove.y,
-      });
-    }
-  };
-  const handleMouseUp = () => {
-    setIsDragging(false);
-  };
+
   return (
     <Dialog open={open || true} onOpenChange={onOpenChange || closeModal}>
       <DialogContent
-        style={{
-          transform: `translate(${position.x}px, ${position.y}px)`,
-        }}
         className={`overflow-y-auto max-h-[80vh]  w-full ${maxWidth}`}>
         {(title || description) && (
-          <DialogHeader
-            style={{
-              cursor: isDragging ? "grabbing" : "grab",
-            }}
-            onMouseDown={handleMouseDown}
-            onMouseMove={handleMouseMove}
-            onMouseUp={handleMouseUp}>
+          <DialogHeader>
             {title && <DialogTitle>{title}</DialogTitle>}
             {description && (
               <DialogDescription>{description}</DialogDescription>
