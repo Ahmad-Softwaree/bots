9b7521e5febfe06ea99af7d2a8e8f736563db1e0
Ahmad-Software <dr.ahmad.salah.54@gmail.com>
2026-01-13 20:05:04 +0300

    feat: implement QueryProvider for improved data fetching and state management

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,33 +1,20 @@
 "use client";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useState } from "react";
 import { ThemeProvider } from "@/providers/theme-provider";
 import LanguageProvider from "@/providers/language-provider";
 import { NuqsAdapter } from "nuqs/adapters/next/app";
 import { ClerkProvider } from "@clerk/nextjs";
 import { shadcn } from "@clerk/themes";
+import { QueryProvider } from "@/providers/query-provider";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  const [queryClient] = useState(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            staleTime: 60 * 1000,
-            refetchOnWindowFocus: false,
-          },
-        },
-      })
-  );
-
   return (
     <ClerkProvider
       afterSignOutUrl={"/"}
       appearance={{
         baseTheme: shadcn,
       }}>
-      <QueryClientProvider client={queryClient}>
+      <QueryProvider>
         <NuqsAdapter>
           <LanguageProvider>
             <ThemeProvider
@@ -39,7 +26,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
             </ThemeProvider>
           </LanguageProvider>
         </NuqsAdapter>
-      </QueryClientProvider>
+      </QueryProvider>
     </ClerkProvider>
   );
 }
